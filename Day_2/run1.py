4602792a838902b2e44775ab09ff9397bc349bb1
Professor Xplorer <px@Professors-MacBook-Pro.local>
2024-09-13 17:24:12 +0530

    day2

---
+++ Day_2/run1.py
@@ -0,0 +1,22 @@
+x = "5" #string
+z1= 5 # int
+y = True #bool
+z = ["a","b"] #list
+a = {'apple':"px"} #dictionary 
+b = 1j #complex
+c = ('apple','px') # tuple
+d = {'apple','px'} # set
+e = b"hello" # Bool
+f = None #none
+g = range(10) #range
+print(c)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
