56568da7edfe0c5e0fd4162e312c918c8b12f888
Professor Xplorer <px@Professors-MacBook-Pro.local>
2024-09-13 16:53:16 +0530

    dayy1

---
+++ myFirstCode.py
@@ -0,0 +1,62 @@
+# print("Taking input from CLI")
+# inputeName = input("Please enter your name: ")
+# print(inputeName)
+
+
+# if 5 > 2:
+#     # If condition is true
+#     print("5 is greater then 2")
+# else:
+#     print("5 is not greater then 6")
+
+
+
+#Varibale
+
+# x = 5
+# y = "Gabe"
+# print(x)
+# print(y)
+
+# z = str(x)
+# print(type(x))
+
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# print(z)
+
+# Which is NOT a legal variable name?
+
+# my-var = 20
+
+# my_var = 20
+
+# Myvar = 20
+
+# _myvar = 20
+
+
+
+# Create a variable named carname and assign the value Volvo to it.
+
+
+
+carName = "Volvo"
+
+
+# Create a variable named x and assign the value 15 to it.
+
+x = 15
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
