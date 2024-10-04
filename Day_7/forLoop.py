c4c8ac58a53391f60d26d2fcc832f91a6b1feef9
Professor Xplorer <px@Professors-MacBook-Pro.local>
2024-10-04 17:09:11 +0530

    git

---
+++ Day_7/forLoop.py
@@ -0,0 +1,13 @@
+# gabe = [1,2,3,4,57,0,11,32,22]
+
+# for i in gabe:
+#     print(i)
+
+# for x in "banana":
+#   print(x)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits :
+    print(fruit)
---
+++ Day_7/ifelse.py
@@ -0,0 +1,12 @@
+a = 33
+b = 20
+c = 99
+
+if b > a:
+  print("b is greater than a")
+elif c < a:
+    print(" c is greater then a")
+elif c < b:
+    print(" c is greater then a")
+else :
+  print("b is not greater than a")
